679124f25497ad686ba7183b952daa41110ccbaa
Adam-Teo <adam.teodorsson@iths.se>
2024-11-21 10:56:28 +0100

    Update : Added a new graph to my personal file

--- adam.py
+++ adam.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import plotly_express as px
-from dash import html, dcc, dash_table
+from dash import html, dcc
 import dash_bootstrap_components as dbc
 
 # Create your charts here 
@@ -11,9 +11,63 @@ def select_sport(selection_of_sport):
     medals_by_country = (df[chosen_sport].groupby("NOC")[["Medal"]].count().sort_values(by="Medal",ascending=False).reset_index()) # groups by NOC and counts number of medals, sort values and then resets index.
     return medals_by_country
 
+# ----------------
+# Unique_Medals
+
+hungary = df[df["NOC"]=="HUN"]
+
+def  unique_medals( df ):
+    set_year_medal_event = set()
+    for idx, row in df.dropna( subset=["Medal"] ).iterrows():
+        set_year_medal_event.add(f" {row['Year']}|{row['Event']}|{row['Medal']} ")
+    len(set_year_medal_event)
+
+    year = [ row.split("|")[0][1:] for row in set_year_medal_event ]
+    event = [ row.split("|")[1] for row in set_year_medal_event ]
+    medal = [ row.split("|")[2][:-1] for row in set_year_medal_event ]
+
+    df_new = pd.DataFrame(   
+        {"Year":year, "Medal":medal, "Event":event}
+    ) 
+
+    return df_new
+
+# Hungary Unique Medal Per Event
+hungary_ume = unique_medals( hungary )
+
+hungary_ume
+
+
+# ----------------
+# Medals Only
+def medals_only( df ):
+    df.groupby("Year")["Medal"].count()
+    df["Gold"] = df["Medal"] == "Gold" 
+    df["Silver"] = df["Medal"] == "Silver" 
+    df["Bronze"] = df["Medal"] == "Bronze" 
+
+    return df[["Year","Gold","Silver","Bronze"]].groupby("Year").sum()
+
+hungary_medals = medals_only( hungary_ume  )
+hungary_medals
+
+
+# ----------------
+
+# Example Dataframe
 df_info = pd.DataFrame() 
 df_info["Hosts"] = "Athens,France,USA,UK,Sweden".split(",")
 df_info["Year"] = "1896,1900,1904,1908,1912".split(",")
+
+# I stored the px.line in a variable outside the select dict
+# because dash doesn't like that you add .uppdate_layout 
+# while you add the graph to the figure element
+unique_medals = px.line(
+    hungary_medals, y=["Gold", "Silver", "Bronze"], 
+    color_discrete_sequence=['Gold', 'Silver', 'Brown']
+    ).update_layout(yaxis_title="Number of Medals")
+
+
 # Add your completed charts to the select dictionary
 # select={ 
 #   "chart_name_a": dcc.Graph( figure=px.bar(    )), 
@@ -22,7 +76,8 @@ df_info["Year"] = "1896,1900,1904,1908,1912".split(",")
 # the dcc.Graph is the element used by app.layout = [] to display graphs
 # the figure= contains the plotly_express chart
 # check the tobbe.py file for further references
-# NOTE You should be able to add any tipe of object like a string, dash_table or another div
+# NOTE You should be able to add any tipe of object like a string, dash_table or another di
+
 select={
         "Sailing": dcc.Graph( figure=px.bar(
             select_sport("Sailing"),
@@ -38,6 +93,7 @@ select={
             color="NOC",
             title= f"Medal overview in Canoeing"
          )),
+         "Unique Medals": dcc.Graph( figure= unique_medals ),
          #Project-OS\olympic_flag_2.png
          #Project-OS\olympic_flag_2.png
          
