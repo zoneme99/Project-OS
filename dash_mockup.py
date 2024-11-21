28d433781ded4d975ad2d0e08926dae7e564d04b
AlexanderTarler <alexander.tarler@gmail.com>
2024-11-21 13:55:12 +0100

    Added an assets folder for Dash to use a css-file as well as made some design changes to the dashboard

--- dash_mockup.py
+++ dash_mockup.py
@@ -1,52 +1,89 @@
 import pandas as pd
 import plotly_express as px
-from dash import Dash,html, dcc, dash_table
+from dash import Dash, html, dcc, dash_table
 from dash.dependencies import Input, Output
 import dash_bootstrap_components as dbc
-import adam, alex, jonte, tobbe
+import adam
+import alex
+import jonte
+import tobbe
 
-app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])                                        # Initialize app
+# Initialize app
+app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
-# Imports the select dictionary from each persons file and 
-# extrackts the key and coverts it a lable value pair that 
+# Imports the select dictionary from each persons file and
+# extrackts the key and coverts it a lable value pair that
 # the drop down menu can use
-options_list = list() 
-options_list += [ {"label":key, "value":key} for key in adam.select.keys() ] 
-options_list += [ {"label":key, "value":key} for key in alex.select.keys() ] 
-options_list += [ {"label":key, "value":key} for key in jonte.select.keys() ] 
-options_list += [ {"label":key, "value":key} for key in tobbe.select.keys() ] 
+options_list = list()
+options_list += [{"label": key, "value": key} for key in adam.select.keys()]
+options_list += [{"label": key, "value": key} for key in alex.select.keys()]
+options_list += [{"label": key, "value": key} for key in jonte.select.keys()]
+options_list += [{"label": key, "value": key} for key in tobbe.select.keys()]
 
-app.layout = [                                              # layout-design
-    html.Div(children = "Project OS", style={"text-align": "center","font-size": "40px", "font-weight": "bold", "color": "#4CAF50"}),
-    html.Hr(),
-    dcc.Dropdown(                                                                       # Dropdown on dash, with labels and values.
-    id="Sport dropdown",
-    options = options_list,
-    #options = [
-    #        {"label": "Weightlifting", "value": "Weightlifting"},
-    #        {"label": "Archery", "value": "Archery"},
-    #        {"label": "Gymnastics", "value": "Gymnastics"},
-    #        {"label": "Age distribution with medals", "value": "Age distribution"}
-    #        ],
+app.layout = html.Div(
+    style={
+        'backgroundColor': '#FBE9D1',
+        'height': '100vh',
+        'display': 'flex',
+        'flexDirection': 'column',
+    },
+    children=[
+        html.Div(
+            children=[
+                html.Div(
+                    "Project OS",
+                    style={
+                        "text-align": "center",
+                        "font-size": "40px",
+                        "font-weight": "bold",
+                        "color": "#444339",
+                    }
+                ),
+                html.Hr(),
+                dcc.Dropdown(
+                    id="Sport-dropdown",
+                    options=options_list,
+                    placeholder="Select a sport",
+                    style={
+                        "width": "20vw",
+                        "margin": "auto",
+                        "padding": "0px"
+                    },
+                    value="Medal Distribution For Hungary"
+                ),
 
-    placeholder="Select a sport", 
-    style={"width": "45%", "margin": "auto", "padding": "10px"},     # Gives dropdown a "default text"
-    ),
-    #dcc.Graph( id="Medal chart", ),    
-    html.Div( id="Div chart", children = {}  )
-]
+            ],
+            style={
+                'flex': '0 0 auto',
+                'padding': '20px',
+                'backgroundColor': '#F1F0EB'
+            }
+        ),
+        html.Div(
+            id="Div chart",
+            children={},
+            style={
+                'flex': '1 1 auto',
+                'padding': '20px',
+                'backgroundColor': '#F1F0EB'
+            }
+        )
+    ]
+)
+
 
 @app.callback(                                                                  # Gives the dashapp input and output, user input linked with app output.
     Output("Div chart", "children"),
-    Input("Sport dropdown", "value")
+    Input("Sport-dropdown", "value")
 )
 def medal_chart(selection_of_sport):
 
-    if not selection_of_sport:    
-        return {}                                              # if no choice - show empty graph.
-        #return px.bar(title="")
-    
-    # Imports the select dictionary that contains the 
+    if not selection_of_sport:
+        # if no choice - show empty graph.
+        return {}
+        # return px.bar(title="")
+
+    # Imports the select dictionary that contains the
     # charts from each persons file
     select = dict()
     select.update(adam.select)
@@ -70,9 +107,8 @@ def medal_chart(selection_of_sport):
             title= f"Medal overview in Archery"
          ),
     """
-    #return select[selection_of_sport]
+    # return select[selection_of_sport]
     return (select[selection_of_sport],)
-
 
 
 if __name__ == "__main__":                                  # Runs the app
