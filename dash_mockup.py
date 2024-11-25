286ce4f2f6b6c6a9f61b9470ad10a6bcb3792b48
AlexanderTarler <alexander.tarler@gmail.com>
2024-11-25 11:28:24 +0100

    Fixed an issue where the dropdown menu did not allow the user to switch graphs

--- dash_mockup.py
+++ dash_mockup.py
@@ -1,19 +1,13 @@
-# Används inte.. Tabort?
-# [
-import pandas as pd
-import plotly.express as px
-# ] 
-from dash import Dash, html, dcc, dash_table
-from dash.dependencies import Input, Output, State
+from dash import Dash, html, dcc, Input, Output, State
+import dash as dash
 import dash_bootstrap_components as dbc
 import charts
-
 
 app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 server = app.server
 
-
 options_list = [{"label": key, "value": key} for key in charts.select.keys()]
+
 
 app.layout = html.Div(
     style={
@@ -48,18 +42,32 @@ app.layout = html.Div(
                     },
                     value="Hungary Overview"
                 ),
-                html.Div(className='dbc',
-                         style={"display": "flex",
-                                "justifyContent": "center", "margin-top": "20px"},
-                         children=[
-                             dbc.Button(
-                                 'Previous', id='prev-button', n_clicks=0, style={"backgroundColor": "#EFE1BA", "color": "#444339", 'border': '2px solid #444339',
-                                                                                  'border-radius': '6px', }),
-                             dbc.Button('Next', id='next-button',
-                                        n_clicks=0, style={"margin-left": "10px", "backgroundColor": "#EFE1BA", "color": "#444339", 'border': '2px solid #444339',
-                                                           'border-radius': '6px', }),
-                         ]
-                         ),
+                html.Div(
+                    className='dbc',
+                    style={"display": "flex",
+                           "justifyContent": "center", "margin-top": "20px"},
+                    children=[
+                        dbc.Button(
+                            'Previous', id='prev-button', n_clicks=0,
+                            style={
+                                "backgroundColor": "#EFE1BA",
+                                "color": "#444339",
+                                'border': '2px solid #444339',
+                                'border-radius': '6px'
+                            }
+                        ),
+                        dbc.Button(
+                            'Next', id='next-button', n_clicks=0,
+                            style={
+                                "margin-left": "10px",
+                                "backgroundColor": "#EFE1BA",
+                                "color": "#444339",
+                                'border': '2px solid #444339',
+                                'border-radius': '6px'
+                            }
+                        ),
+                    ]
+                ),
             ],
             style={
                 'flex': '0 0 auto',
@@ -83,30 +91,34 @@ app.layout = html.Div(
 
 @app.callback(
     [Output("Div chart", "children"),
-     Output("Sport-dropdown", "value")],
-    Input("chart-index", "data"),
-    State("Sport-dropdown", "value")
+     Output("Sport-dropdown", "value"),
+     Output("chart-index", "data")],
+    [Input("prev-button", "n_clicks"),
+     Input("next-button", "n_clicks"),
+     Input("Sport-dropdown", "value")],
+    State("chart-index", "data")
 )
-def update_chart(chart_index, dropdown_value):
-    if not dropdown_value:
-        dropdown_value = "Hungary Overview"
-
+def update_chart(prev_clicks, next_clicks, dropdown_value, current_index):
     select = charts.select
     keys = list(select.keys())
 
-    chart_index = chart_index % len(keys)
-    chart_key = keys[chart_index]
+    ctx = dash.callback_context
 
-    return select.get(chart_key, html.Div("No chart available")), chart_key
+    if ctx.triggered and ctx.triggered[0]["prop_id"] == "Sport-dropdown.value":
 
+        if dropdown_value in keys:
+            current_index = keys.index(dropdown_value)
+    elif ctx.triggered and "prev-button" in ctx.triggered[0]["prop_id"]:
 
-@app.callback(
-    Output("chart-index", "data"),
-    [Input("prev-button", "n_clicks"), Input("next-button", "n_clicks")],
-    State("chart-index", "data")
-)
-def update_index(prev_clicks, next_clicks, current_index):
-    return current_index + (next_clicks - prev_clicks)
+        current_index = (current_index - 1) % len(keys)
+    elif ctx.triggered and "next-button" in ctx.triggered[0]["prop_id"]:
+
+        current_index = (current_index + 1) % len(keys)
+
+    chart_key = keys[current_index]
+    chart = select.get(chart_key, html.Div("No chart available"))
+
+    return chart, chart_key, current_index
 
 
 if __name__ == "__main__":
